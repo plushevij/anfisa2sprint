bb39c1b61b2f937e81729e1fcba0a8f16597081f
Plushevij <zvezdin-misha@mail.ru>
2024-04-20 08:39:49 +0300

    Add some changes in homepage

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values('title', 'id', 'description')
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
